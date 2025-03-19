1a86431079e5b777c79d946d76fd5456d4e4895c
relwarc17 <28979675+Relwarc17@users.noreply.github.com>
2025-03-19 18:46:07 +0100

    Update sensor.py

--- custom_components/marshydro/sensor.py
+++ custom_components/marshydro/sensor.py
@@ -43,7 +43,8 @@ SCAN_INTERVAL = timedelta(seconds=5)
 class MarsHydroSensor(MarsHydroEntity, SensorEntity):
     def __init__(self, coordinator, prod_type):
         super().__init__(coordinator, prod_type)
-        self._parent_name = self._coordinator.data[self._device_id]["deviceName"]
+        _LOGGER.debug(f"MarshydroSensor data in coordinator: {str(coordinator.data)}")
+        self._parent_name = coordinator.data[self._device_id]["deviceName"]
 
     async def async_update(self):
         """Update the fan temperature sensor state."""
